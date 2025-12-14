5fc2c5e400cac701a5d72b73284ff5f0e5f84d86
César Baena Abuin <a24cesarba@iessanclemente.net>
2025-12-14 01:07:06 +0100

    errores

--- kea/entrypoint.sh
+++ kea/entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 
-ip addr del decffault via 192.168.10.1
-ip addr del decffault via 192.168.10.254
+ip route del default via 192.168.10.1
+ip route del default via 192.168.10.254
 
 rm -f /var/run/kea/kea-dhcp4.kea-dhcp4.pid
 rm -f /var/run/kea/kea-dhcp-ddns.kea-dhcp-ddns.pid
 
-kea-dhcp4 -c /etc/kea/dhcp4.conf -d #&
+kea-dhcp-ddns -d -c /etc/kea/kea-dhcp-ddns.conf  &
+kea-dhcp4 -c /etc/kea/kea-dhcp4.conf -d 
 
 # sleep infinity
